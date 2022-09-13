3af87b696c9cc7f890b68c42483e49ec681c4671
Georgey1127 <110436992+Georgey1127@users.noreply.github.com>
2022-09-13 09:23:41 -0500

    Add files via upload

---
+++ Cascaron.py
@@ -0,0 +1,20 @@
+class Librarian:
+    def __init__(self, book_list: list):
+        pass
+
+    def addB(self, book):
+        pass
+    def removeB(self, book):
+        pass
+    def display(self):
+        pass
+
+class User:
+    def __init__(self, name: str):
+        pass
+
+    def borrowBook(self, book: str, booklist: list):
+        pass
+    
+    def returnBook(self, book: str, booklist: list):
+        pass
